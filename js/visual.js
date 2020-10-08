e96a69dd84e83786776b310e20da60e3cbb2a3a5
Chen Zeng <chen.zeng@centre.edu>
2020-10-08 09:44:32 +0800

    added call to action and visualization

---
+++ js/visual.js
@@ -0,0 +1,14 @@
+d3.select("body")
+.append("img")
+.attr("class","gChart")
+.attr("src","img/geoCharts.png")
+
+d3.select("body")
+.append("img")
+.attr("class","dD")
+.attr("src","img/deathDensity.png")
+
+d3.select("body")
+.append("img")
+.attr("class","noDC")
+.attr("src","img/filterNoDC.png")
